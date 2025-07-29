d526b88f7c2b8c0f40f278fdfad1e9fa2f29f03f
janricayp <janricayp@gmail.com>
2025-07-29 20:13:36 +0800

    Add day 1: basic data types and Euclidean distance example

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,41 @@
+import math
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 % 4)
+print(3 ** 4)
+print(3 // 4)
+
+print("Jan")
+print("Ybanez")
+print("Philippines")
+print("I am enjoying 30 Days of Python")
+
+int_number = 3
+float_number = 3.14
+complex_number = 3 + 4j
+boolean_type = True
+list_type = [1, 2, 3, 4, 5]
+tuple_type = (1, 2, 3, 4, 5)
+set_type = {1, 2, 3, 4, 5}
+dictionary_type = {"name": "Jan", "age": 30, "country": "Philippines"}
+string_type = "I am enjoying 30 Days of Python"
+
+print(int_number)
+print(float_number)
+print(complex_number)
+print(string_type)
+print(boolean_type)
+print(list_type)
+print(tuple_type)
+print(set_type)
+print(dictionary_type)
+
+x1 = 2
+y1= 3
+x2 = 10
+y2 = 8
+euclidean_distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+print(round(euclidean_distance, 2))
