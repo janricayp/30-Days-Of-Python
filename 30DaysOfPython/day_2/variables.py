1d18e8db8a65bd6cd621041c4854803156371ecd
janricayp <janricayp@gmail.com>
2025-07-30 16:34:13 +0800

    Add Day 2: variables and data types practice in Python

--- 30DaysOfPython/day_2/variables.py
+++ 30DaysOfPython/day_2/variables.py
@@ -1,5 +1,6 @@
 # Day 2: 30 Days of python programming
 
+import math
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 full_name = f"{first_name} {last_name}"
@@ -21,7 +22,7 @@ print(type(age))         # <class 'int'>
 print(type(year))        # <class 'int'>
 print(type(is_married))  # <class 'bool'>
 print(type(is_true))     # <class 'bool'>
-print(type(is_light_on)) # <class 'bool'>
+print(type(is_light_on))  # <class 'bool'>
 print(type(number_1))    # <class 'int'>
 print(type(number_2))    # <class 'int'>
 print(type(number_3))    # <class 'int'>
@@ -53,10 +54,11 @@ floor_division = num_one // num_two
 print(f"Floor Division: {floor_division}")  # Floor Division: 1
 
 # Area and Circumference of a Circle
-import math
 radius = input("Enter the radius of the circle: ")
 radius = float(radius)  # Convert input to float
 area_of_circle = math.pi * (radius ** 2)
 circum_of_circle = 2 * math.pi * radius
-print(f"Area of Circle: {area_of_circle}")  # Area of Circle: 2827.4333882308138
-print(f"Circumference of Circle: {circum_of_circle}")  # Circumference of Circle: 188.49555921538757
+# Area of Circle: 2827.4333882308138
+print(f"Area of Circle: {area_of_circle}")
+# Circumference of Circle: 188.49555921538757
+print(f"Circumference of Circle: {circum_of_circle}")
